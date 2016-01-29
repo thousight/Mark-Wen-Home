dac4171cd025fed7d04a4c9dafe917217db02b30
Mark Wen <markwenguojie94@gmail.com>
2016-01-29 00:51:22 -0500

    v1.7 alpha
    Finished education page (hopefully)
    added timeline
    Finishing experience page
    added timeline
    working on overflow scrolling

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-/* ---------------------------------------- Main Body Stuff ----------------------------------------*/
+/* ======================================== Main Body Stuff ========================================*/
 
 // Page scroll animation function
 var pageScroll = function(element){
@@ -13,7 +13,7 @@ $('.NextPageBtn').click(function(e){
   pageScroll('#' + $(this).parent().parent().next().attr('id'));
 })
 
-/* ---------------------------------------- Navigation Bar -----------------------------------------*/
+/* ======================================== Navigation Bar =========================================*/
 
 // Select navbar items so that when clicked it scrolls the page to destination
 $('.navbar-items').click(function(e){
@@ -30,57 +30,73 @@ $('.nav a').click(function(){
 // Navbar item can become active when the page is at its corresponding page
 // Variables
 var lastId,
-    NavMenu = $("#navbar-menu"),
-    NavMenuHeight = NavMenu.outerHeight()+15,
-    // All list items
-    menuItems = NavMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
+NavMenu = $("#navbar-menu"),
+NavMenuHeight = NavMenu.outerHeight()+15,
+// All list items
+menuItems = NavMenu.find("a"),
+// Anchors corresponding to menu items
+scrollItems = menuItems.map(function(){
+  var item = $($(this).attr("href"));
+  if (item.length) { return item; }
+});
 // Bind to scroll
 $(window).scroll(function(){
-   // Get container scroll position
-   var fromTop = $(this).scrollTop()+NavMenuHeight;
-   // Get id of current scroll item
-   var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
-       return this;
-   });
-   // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
-   if (lastId !== id) {
-       lastId = id;
-       // Set/remove active class
-       menuItems
-         .parent().removeClass("active")
-         .end().filter("[href='#"+id+"']").parent().addClass("active");
-   }
+  // Get container scroll position
+  var fromTop = $(this).scrollTop()+NavMenuHeight;
+  // Get id of current scroll item
+  var cur = scrollItems.map(function(){
+    if ($(this).offset().top < fromTop)
+    return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length-1];
+  var id = cur && cur.length ? cur[0].id : "";
+  if (lastId !== id) {
+    lastId = id;
+    // Set/remove active class
+    menuItems
+    .parent().removeClass("active")
+    .end().filter("[href='#"+id+"']").parent().addClass("active");
+  }
 });
 
 
-/* ------------------------------------------- HomePage --------------------------------------------*/
+/* =========================================== HomePage ============================================*/
 
 // Code here...
 
 
-/* ------------------------------------------ Education --------------------------------------------*/
+/* ========================================== Education ============================================*/
+
+// Code here...
+
+/* ========================================== Experience ===========================================*/
+
+// Biding checkScroll() to scroll
+$(document).ready(function(){
+  $('div').bind('scroll', checkScroll);
+});
+
+// Check if it is scrolled to the top or bottom of the div
+function checkScroll(e) {
+   $('.expTimeline').scroll(function () {
+    if ( $('.expTimeline').scrollTop() == 0) {
+      console.log('Scrolled to Page Top');
+      $(window).scroll();
+    }
+    else if ( $(e.currentTarget)[0].scrollHeight - $(e.currentTarget).scrollTop() == $(e.currentTarget).outerHeight()) {
+      console.log('Scrolled to Page Bottom');
+      pageScroll(window);
+    }
+  });
+}
+
+
+/* ============================================ Skills =============================================*/
 
 // Code here...
 
 
-/* ------------------------------------------ Experience -------------------------------------------*/
-
-// Code here...
-
-
-/* -------------------------------------------- Skills ---------------------------------------------*/
-
-// Code here...
-
-
-/* ------------------------------------------ Contact Me -------------------------------------------*/
+/* ========================================== Contact Me ===========================================*/
 
 // Code here...
